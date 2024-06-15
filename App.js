ae8421b0d88570bcd27172d8bca58ba442731d75
Rahul kumar <rahulofficial3200@gmail.com>
2024-06-15 19:13:28 +0530

    functional component in react

--- App.js
+++ App.js
@@ -1,4 +1,46 @@
-const heading =React.createElement("h1",{id:"heading"},"i am the heading");
-
+import React from "react";
+import ReactDOM from "react-dom";
+//this was simply using react element
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// const heading=(<h1 className="heading">
+//     lets see namste react<br></br>
+//     namaste react using jsx</h1>);
+//     root.render(heading);//for render react elements
+// this was using react functional component
+         // const HeadingComponent=()=>{
+         //     return <h1>namaste react functional componenet</h1>
+         // } can be written as:
+
+
+//component composition  
+
+const subtitle=<p>i am subtitle</p>
+ 
+const title= (<h1 className="title">
+    {subtitle}
+    i am ssamll title</h1>);
+ 
+
+ const Title=()=>(
+    <h1 className="head">
+        namste dostoooo
+    </h1>
+);
+
+const number =100
+const number2=500
+ //you can insert any piece of js inside your function ({})
+
+
+const HeadingComponent=()=> (
+    <div id="box">
+        {number}
+        <h2>{number2}</h2>
+        {title}
+        <Title/>  
+       <h1 className="heading">namaste react functional component</h1>
+</div>
+);
+
+root.render(<HeadingComponent/>);//render for function 
+
